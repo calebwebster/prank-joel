b5718171a511e55e6cb69ce3774762d0a4a2128d
Caleb Webster <caleb.webster@my.jcu.edu.au>
2021-01-11 16:42:37 +1000

    Remove chances.txt Remove code to generate random number

--- edit_batch_file.py
+++ edit_batch_file.py
@@ -1,5 +1,4 @@
 import requests
-import random
 
 COMMANDS = """
 @echo off
@@ -11,13 +10,8 @@ enabled = requests.get('https://raw.githubusercontent.com/CalebWebsterJCU/GayApe
 enabled.raise_for_status()
 
 if enabled.text == 'True':
-    chances = requests.get('https://raw.githubusercontent.com/CalebWebsterJCU/GayApe/master/chances.txt')
-    chances.raise_for_status()
-    rand_num = int(chances.text)  # Odds of Gay Ape running, e.g. 10 = 1 in 10 chance
-    if random.randint(1, rand_num) == 1:
-        # Write commands to batch file
-        with open('C:\\GayApe\\run_gay_ape.bat', 'w') as batch_file:
-            print(COMMANDS, file=batch_file)
+    with open('C:\\GayApe\\run_gay_ape.bat', 'w') as batch_file:
+        print(COMMANDS, file=batch_file)
 else:
     # Erase batch file
     with open('C:\\GayApe\\run_gay_ape.bat', 'w') as batch_file:
