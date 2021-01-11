1ca69d90b3facc97eb55ecdf98aaf7a6789944e1
Caleb Webster <caleb.webster@my.jcu.edu.au>
2021-01-11 17:38:35 +1000

    Remove inputs

--- edit_batch_file.py
+++ edit_batch_file.py
@@ -3,7 +3,7 @@ import requests
 COMMANDS = """
 @echo off
 cd C:\\GayApe
-gay_ape.exe
+run.exe
 """
 
 enabled = requests.get('https://raw.githubusercontent.com/CalebWebsterJCU/GayApe/master/enabled.txt')
@@ -13,11 +13,7 @@ if enabled.text == 'True':
     # Write commands
     with open('C:\\GayApe\\run.bat', 'w') as batch_file:
         print(COMMANDS, file=batch_file)
-    print('Batch file written')
-    input()
 else:
     # Erase batch file
     with open('C:\\GayApe\\run.bat', 'w') as batch_file:
         print('', file=batch_file)
-    print('Batch file erased')
-    input()
